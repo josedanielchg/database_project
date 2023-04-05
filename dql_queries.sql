0901b79f592d28971ade44e0e03f656a910bb725
Gabriel Díaz <gda.m1712@gmail.com>
2023-04-05 03:42:34 -0400

    Added simple order by queries

--- dql_queries.sql
+++ dql_queries.sql
@@ -40,6 +40,21 @@ select * from polls where start_date < '2023-05-01 00:00:00';
 /* Return the products with price < 10 dollars */
 select * from products where price::numeric::float8 < 10.00;
 
+/* Return the users ordered by email */
+select * from users order by email;
+
+/* Return the most recent 10 polls */
+select * from polls order by end_date desc limit 10;
+
+/* returns the restaurants ordered from the one that closes later */
+select * from restaurants order by closing_time desc;
+
+/* returns the products ordered from the cheapest */
+select * from products order by price limit 10;
+
+/* Return the users ordered by name */
+select * from food_types order by name limit 10;
+
 /* SELECT QUERIES WITH JOIN */
 
 /* return all products with the restaurant name */
