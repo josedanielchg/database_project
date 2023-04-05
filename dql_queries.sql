ea26b4f346f5cb5b6ff9b8e0b91c1cde99320be4
Gabriel Díaz <gda.m1712@gmail.com>
2023-04-05 03:28:14 -0400

    Added more dml simple queries

--- dql_queries.sql
+++ dql_queries.sql
@@ -28,6 +28,18 @@ select max(price) as price, (select f.name from food_types f where id = food_typ
 /* Return to the restaurants that are open at this moment */
 select * from restaurants r where r.opening_time < CURRENT_TIME and r.closing_time > CURRENT_TIME;
 
+/* Return the restaurant info in uppercase */
+select id, upper(r.name) from restaurants r;
+
+/* Return the product with specific format */
+select concat(p.name, ' tiene un precio de: ', p.price) as product_price from products p where p.restaurant_id = 2;
+
+/* Return the polls that started before 2023-05 */
+select * from polls where start_date < '2023-05-01 00:00:00';
+
+/* Return the products with price < 10 dollars */
+select * from products where price::numeric::float8 < 10.00;
+
 /* SELECT QUERIES WITH JOIN */
 
 /* return all products with the restaurant name */
@@ -81,3 +93,4 @@ select ft.name, count(*)
          inner join food_types ft on (p.food_type_id = ft.id)
     where p.price::numeric::float8 < 10.00
     group by ft.name
+
