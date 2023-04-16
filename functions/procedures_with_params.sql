ad82c0eeba4658a5ff687c716f8fe680ebb23130
josedanielchg <j.daniel.chg@gmail.com>
2023-04-16 13:24:41 -0400

    Translate to english all comments in procedures_with_params.sql

--- functions/procedures_with_params.sql
+++ functions/procedures_with_params.sql
@@ -1,33 +1,38 @@
-/* Que permita crear poll y opciones asociadas, recibe por parametro: start_date del poll, end_date del poll, el nombre del poll y los id's de los restaurantes. */
-CREATE OR REPLACE PROCEDURE insert_poll(
-    start_date TIMESTAMP, end_date TIMESTAMP, name varchar,
-    restaurant_id_1 INTEGER, restaurant_id_2 INTEGER)
-AS $$
-DECLARE
-    poll_id INTEGER;
-BEGIN
+/* Create polls and associated options, receives by parameter: 
+    - start_date of the poll, 
+    - end_date of the poll, 
+    - the name of the poll 
+    - the id's of the restaurants.
+*/
+create or replace procedure insert_poll(
+    start_date timestamp, end_date timestamp, name varchar,
+    restaurant_id_1 integer, restaurant_id_2 integer)
+as $$
+declare
+    poll_id integer;
+begin
     -- Insert new poll in polls table
-    INSERT
-    INTO polls (start_date, end_date, name)
-    VALUES(start_date, end_date, name)
-    RETURNING id INTO poll_id;
+    insert
+    into polls (start_date, end_date, name)
+    values(start_date, end_date, name)
+    returning id into poll_id;
 
     -- Insert poll_restaurants
-    INSERT
-    INTO poll_restaurants(poll_id, restaurant_id, is_winner)
-    VALUES(poll_id, restaurant_id_1, false);
+    insert
+    into poll_restaurants(poll_id, restaurant_id, is_winner)
+    values(poll_id, restaurant_id_1, false);
 
-    INSERT
-    INTO poll_restaurants(poll_id, restaurant_id, is_winner)
-    VALUES(poll_id, restaurant_id_2, false);
-END;
-$$ LANGUAGE plpgsql;
+    insert
+    into poll_restaurants(poll_id, restaurant_id, is_winner)
+    values(poll_id, restaurant_id_2, false);
+end;
+$$ language plpgsql;
 
-CALL insert_poll('2023-04-16 09:00:00', '2023-04-23 09:00:00', 'Ejemplo encuesta profesor', 1, 2);
+call insert_poll('2023-04-16 09:00:00', '2023-04-23 09:00:00', 'Ejemplo encuesta profesor', 1, 2);
 
 
 
-/* Retorna los restaurantes que estan abiertos antes del end_date de un poll (recibe el poll_id y retorna una lista de restaurantes). */
+/* Returns the restaurants that are open before the end_date of a poll (receives the poll_id and returns a list of restaurants). */
 create or replace function get_open_restaurants(poll_id int)
     returns table(
      id int,
@@ -53,7 +58,7 @@ select * from get_open_restaurants(1);
 
 
 
-/* Retorna un listado de usuarios que han comprado en un restaurante (recibe el restaurant_id). */
+/* Returns a list of users who have purchased at a restaurant (receives the restaurant_id) */
 create or replace function get_restaurant_clients(id_restaurant int)
     returns table(
 	 id integer,
